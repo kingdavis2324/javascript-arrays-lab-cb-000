5b3cc33bf4bd6736e6c73626a644fe35c0e0aa59
Kenny Davis <kjdavis2423@gmail.com>
2018-04-18 00:37:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name){
   kittens.push(name)
 }
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(){
+  
+}
